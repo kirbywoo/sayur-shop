08e55f5dbc2ba7126aa0640b4f6c977780f0ea7d
kirbywoo <eva.yunia110904@gmail.com>
2024-09-06 22:42:41 +0700

    urls routing config

---
+++ main/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
+    stock = models.IntegerField()
+    image = models.ImageField()
