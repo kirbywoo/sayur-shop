09701d4120e53d0f60c3e2ccc33db6acf8a7b85e
kirbywoo <eva.yunia110904@gmail.com>
2024-09-18 02:12:29 +0700

    routing url

--- main/models.py
+++ main/models.py
@@ -1,8 +1,16 @@
+import uuid
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
     stock = models.IntegerField()
-    image = models.ImageField()
+    rating = models.DecimalField(
+        max_digits=2, 
+        decimal_places=1, 
+        default=0.0, 
+        validators=[MinValueValidator(0.0), MaxValueValidator(5.0)],
+    )
