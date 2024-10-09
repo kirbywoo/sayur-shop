14710a63b9fa858365a8fc8ce50b2624385d1e3a
kirbywoo <eva.yunia110904@gmail.com>
2024-10-09 09:45:29 +0700

    add ajax

--- main/views.py
+++ main/views.py
@@ -11,15 +11,16 @@ from django.contrib.auth.decorators import login_required
 import datetime
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.html import strip_tags
 
 @login_required(login_url='/login')
 def show_main(request):
-    product_entries = Product.objects.filter(user=request.user)
 
     context = {
         'name': request.user.username,
         'class': 'PBP C',
-        'product_entries' : product_entries,
         'last_login': request.COOKIES['last_login'],
     }
 
@@ -38,11 +39,11 @@ def create_product_entry(request):
     return render(request, "create_product_entry.html", context)
 
 def show_xml(request):
-    data = Product.objects.all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = Product.objects.all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, id):
@@ -75,7 +76,8 @@ def login_user(request):
             response = HttpResponseRedirect(reverse("main:show_main"))
             response.set_cookie('last_login', str(datetime.datetime.now()))
             return response
-
+      else:
+          messages.error(request, "Invalid username or password. Please try again.")
    else:
       form = AuthenticationForm(request)
    context = {'form': form}
@@ -109,3 +111,28 @@ def delete_product(request, id):
     product.delete()
     # Kembali ke halaman awal
     return HttpResponseRedirect(reverse('main:show_main'))
+
+@csrf_exempt
+@require_POST
+def add_product_entry_ajax(request):
+    name = strip_tags(request.POST.get("name"))
+    price = request.POST.get("price")
+    description = strip_tags(request.POST.get("description"))
+    stock = request.POST.get("stock")
+    rating = request.POST.get("rating",0.0)
+    user = request.user
+
+    new_product = Product(
+        name=name, price=price,
+        description=description,
+        stock=stock, rating=rating,
+        user=user
+    )
+    
+    form = ProductEntryForm(request.POST, instance=new_product)
+    if form.is_valid():
+        new_product.save()
+        return HttpResponse(b"CREATED", status=201)
+    else:
+        errors = form.errors.as_json()
+        return HttpResponse(errors, status=400, content_type="application/json")
