6aa595607e5ffb6e6c7e101fa7c2f30a8ea7a5ce
kirbywoo <eva.yunia110904@gmail.com>
2024-11-20 08:23:09 +0700

    finish tugas 9

--- main/views.py
+++ main/views.py
@@ -14,6 +14,8 @@ from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.utils.html import strip_tags
+import json
+from django.http import JsonResponse
 
 @login_required(login_url='/login')
 def show_main(request):
@@ -137,3 +139,23 @@ def add_product_entry_ajax(request):
         print(form.errors)
         errors = form.errors.as_json()
         return HttpResponse(errors, status=400, content_type="application/json")
+    
+@csrf_exempt
+def create_product_flutter(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body)
+        new_product = Product.objects.create(
+            user=request.user,
+            name=data["name"],
+            description=data["description"],
+            stock=int(data["stock"]),
+            price=int(data["price"]),
+            rating=float(data["rating"]),
+        )
+
+        new_product.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
